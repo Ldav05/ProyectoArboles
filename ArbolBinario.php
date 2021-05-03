2fcdd64745e15e5be59213bb467aea11d29a329f
pico164 <lpicoleon@misena.edu.co>
2021-05-03 10:00:51 -0500

    Creacion del metodo Agregar nodos y Buscar

--- ArbolBinario.php
+++ ArbolBinario.php
@@ -13,6 +13,52 @@
 		}
 
 
+		public function BuscarNodo($nodo,$idNodo){
+			if ($nodo==null) {
+				return null;
+			}else{
+				if ($nodo->GetId()==$idNodo) {
+					return $nodo;
+				}else{
+					$right = $this->BuscarNodo($nodo->GetRight(),$idNodo);
+					$left = $this->BuscarNodo($nodo->GetLeft(),$idNodo);
+
+					if ($right != null) {
+						return $right;
+					}else{
+						return $left;
+					}
+				}
+			}
+		}
+
+
+		public function AgregarNodo($nodo,$pos,$nodoP){
+
+			if ($this->BuscarNodo($nodoP,$nodoP->GetId())) {
+				if ($pos=="I") {
+					$nodoP->SetLeft($nodo);
+				}else{
+					if ($pos=="D") {
+						$nodoP->SetRight($nodo);
+					}
+				}
+			}
+
+		}
+
+
+
+
+		public function ContarNodos($Nodo){
+			if ($Nodo != null) {
+				if ($Nodo) {
+					# code...
+				}
+			}
+		}
+
+
 
 		
 
